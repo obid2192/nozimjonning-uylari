76e82a1094647a5adfb7fa39936b6fe521b6bcbe
KenjayevObid <114725656+KenjayevObid@users.noreply.github.com>
2023-03-20 17:58:45 +0500

    Update style.js

--- src/components/HouseCard/style.js
+++ src/components/HouseCard/style.js
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 100%;
   max-width: 380px;
   min-width: 330px;
