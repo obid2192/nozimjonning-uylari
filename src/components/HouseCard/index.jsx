96ba45d5e21616c44498f562ece8231283e3603e
KenjayevObid <114725656+KenjayevObid@users.noreply.github.com>
2023-03-26 19:41:13 +0500

    Cardaga malumotlar qo'shildi

--- src/components/HouseCard/index.jsx
+++ src/components/HouseCard/index.jsx
@@ -1,5 +1,24 @@
 import CaruselImg from "./caruseImg";
-import { Container, Img} from "./style";
+import {
+  Bank,
+  CardFooter,
+  Container,
+  Content,
+  FooterButton,
+  FooterText,
+  Header,
+  HouseAddress,
+  HousePrice,
+  Img,
+  Line,
+  Metro,
+  MetroIcon,
+  MetroText,
+  MetroTitle,
+  Price,
+  TextCard,
+  Xona,
+} from "./style";
 export const HouseCard = ({ data = {}, gap, onClick }) => {
   // const {
   //   salePrice,
@@ -9,7 +28,7 @@ export const HouseCard = ({ data = {}, gap, onClick }) => {
   //   country,
   //   description,
   //   category,
-    
+
   // } = data;
 
   return (
@@ -18,7 +37,49 @@ export const HouseCard = ({ data = {}, gap, onClick }) => {
       <Img>
         <CaruselImg />
       </Img>
-      
+      <TextCard>
+        <Content>
+          <Bank></Bank>
+          <Header>O'zbegim</Header>
+          <Metro>
+            <MetroIcon>M</MetroIcon>
+            <MetroTitle>Chilonzor</MetroTitle>
+            <MetroText>10 min 1.5 km</MetroText>
+          </Metro>
+          <HouseAddress>
+            Chilonzor tumani Farhod ko'chasi 8 dom 14 uy 2 padez
+          </HouseAddress>
+          <HousePrice>
+            <Xona>1 xona</Xona>
+            <Price>
+              65.45 m<sup>2</sup> ....... 200 000 000 so'm
+            </Price>
+          </HousePrice>
+          <HousePrice>
+            <Xona>1 xona</Xona>
+            <Price>
+              65.45 m<sup>2</sup> ....... 200 000 000 so'm
+            </Price>
+          </HousePrice>
+          <HousePrice>
+            <Xona>1 xona</Xona>
+            <Price>
+              65.45 m<sup>2</sup> ....... 200 000 000 so'm
+            </Price>
+          </HousePrice>
+          <HousePrice>
+            <Xona>1 xona</Xona>
+            <Price>
+              65.45 m<sup>2</sup> ....... 200 000 000 so'm
+            </Price>
+          </HousePrice>
+        </Content>
+        <Line></Line>
+        <CardFooter>
+          <FooterText>Dasturchi Kampaniya</FooterText>
+          <FooterButton>+99899182192 </FooterButton>
+        </CardFooter>
+      </TextCard>
     </Container>
     // </div>
   );
--- src/components/HouseCard/style.js
+++ src/components/HouseCard/style.js
@@ -1,12 +1,6 @@
 import styled from "styled-components";
-import { ReactComponent as bed } from "../../assets/icons/bed.svg";
-import { ReactComponent as bath } from "../../assets/icons/bath.svg";
-import { ReactComponent as garage } from "../../assets/icons/car.svg";
-import { ReactComponent as ruler } from "../../assets/icons/ruler.svg";
-import { ReactComponent as love } from "../../assets/icons/love.svg";
-import { ReactComponent as resize } from "../../assets/icons/resize.svg";
 
-const Container = styled.div`
+export const Container = styled.div`
   position: relative;
   width: 100%;
   max-width: 380px;
@@ -25,65 +19,85 @@ const Container = styled.div`
   cursor: pointer;
 `;
 
-const Img = styled.div`
+export const TextCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-content: space-between;
+`;
+export const Img = styled.div`
   width: 100%;
   height: 210px;
 `;
 
-const Content = styled.div`
+export const Content = styled.div`
+  padding: 10px;
+`;
+export const Header = styled.div`
+  font-size: 25px;
+  font-weight: 600;
+  margin-bottom: 5px;
+`;
+
+export const Metro = styled.div`
+  display: flex;
+  align-items: center;
+`;
+export const MetroIcon = styled.div`
+  color: black;
+  font-weight: 800;
+  font-size: 25px;
+`;
+export const MetroTitle = styled.div`
+  color: red;
+  margin: 0 15px;
+`;
+export const MetroText = styled.div`
+  font-size: 13px;
+  color: gray;
+`;
+export const HousePrice = styled.div`
+  display: flex;
+  align-items: center;
+`;
+export const Xona = styled.div`
+  color: black;
+  font-weight: 600;
+  font-style: 20px;
+`;
+export const Price = styled.div`
+  margin: 0 20px;
+`;
+export const HouseAddress = styled.div`
+  color: gray;
+  margin-bottom: 5px;
+`;
+export const Bank = styled.div`
+  width: 100px;
+  height: 20px;
+  border-radius: 10px;
+  background-color: green;
+`;
+
+export const Line = styled.div`
+width: 100%;
+height: 1px;
+background-color: gray;
+`
+export const CardFooter = styled.div`
+  padding: 10px;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background: white;
+  /* width: 100%; */
   height: 20px;
+  margin-top: 5px;
 `;
-const Content1 = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background: white;
-  height: 20px;
+export const FooterText = styled.div``;
+export const FooterButton = styled.button`
+  outline: none;
+  border: none;
+  border-radius: 3px;
+  height: 25px;
+  width: 120px;
+  background-color: yellow;
 `;
-const Details = styled.div`
-  display: flex;
-  justify-content: space-between;
-  padding-top: 16px;
-`;
-
-Details.Item = styled.div`
-  display: flex;
-  flex-direction: column;
-  flex-direction: ${({ row }) => (row ? "row" : "column")};
-  align-items: ${({ footer }) => !footer && "center"};
-`;
-
-const Icons = styled.div``;
-
-Icons.Bed = styled(bed)``;
-Icons.Bath = styled(bath)``;
-Icons.Garage = styled(garage)``;
-Icons.Ruler = styled(ruler)``;
-Icons.Resize = styled(resize)``;
-Icons.Love = styled(love)`
-  width: 27px;
-  height: 27px;
-  padding: 6px;
-  background: ${({ favorite }) => (favorite ? "red" : "#f6f8f9")};
-  border-radius: 50%;
-  margin-left: 20px;
-  cursor: pointer;
-  & path {
-    fill: ${({ favorite }) => favorite && "white"};
-  }
-  :active {
-    transform: scale(0.9);
-  }
-`;
-
-const Divider = styled.div`
-  background: #e6e9ec;
-  height: 1px;
-  width: 100%;
-`;
-
-export { Container, Img, Content, Content1, Details, Icons, Divider };
